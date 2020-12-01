b376ea387f84e47e16c141b2890a258a7fdb4a8c
Joshua MacSween <joshmacsween@gmail.com>
2020-11-30 17:04:50 -0700

    pt.1 completed

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+//Express Setup
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server started on port: ${PORT}`)
+})
+
+//MongoDB Setup
+mongoose.connect(
+  process.env.MONGODB_CONNECTION_STRING,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err
+    console.log('MongoDB connection established')
+  }
+)
+
+// Routes Setup
+app.use('/users', require('./routes/userRouter'))
