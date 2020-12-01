ea6f6b277543c85352f6bf60216efd324494e739
Joshua MacSween <joshmacsween@gmail.com>
2020-11-30 17:54:05 -0700

    added route and validations

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,7 +1,18 @@
 const router = require('express').Router()
 
-router.get('/test', (req, res) => {
-  res.send("Hello, it's working")
+router.post('/register', async (req, res) => {
+  const { email, password, passwordCheck, displayName } = req.body
+
+  // validation
+
+  if (!email || !password || !passwordCheck)
+    return res.status(400).json({ msg: 'Not all field have been entereds' })
+  if (password.length < 5)
+    return res
+      .status(400)
+      .json({ msg: 'Password must be five characters long' })
+  if (password !== passwordCheck)
+    return res.status(400).json({msg: "Passwords do not match"})
 })
 
 module.exports = router
