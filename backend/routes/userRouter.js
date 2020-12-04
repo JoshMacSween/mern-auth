fa8e218d5249be637dcdc4abd1d5fccc3ea282cf
Joshua MacSween <joshmacsween@gmail.com>
2020-12-03 21:22:46 -0700

    working bcrypt

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const bcrypt = require('bcryptjs')
 const User = require('../models/userModel')
 
 router.post('/register', async (req, res) => {
@@ -8,7 +9,7 @@ router.post('/register', async (req, res) => {
     // validation
 
     if (!email || !password || !passwordCheck)
-      return res.status(400).json({ msg: 'Not all field have been entereds' })
+      return res.status(400).json({ msg: 'Not all field have been entered' })
     if (password.length < 5)
       return res
         .status(400)
@@ -16,13 +17,17 @@ router.post('/register', async (req, res) => {
     if (password !== passwordCheck)
       return res.status(400).json({ msg: 'Passwords do not match' })
 
-    const existingUser = await User.find({ email: email })
+    const existingUser = await User.findOne({ email: email })
     if (existingUser)
       return res
         .status(400)
         .json({ msg: 'An account with this email already exists.' })
 
     if (!displayName) displayName = email
+
+    const salt = await bcrypt.genSalt()
+    const passwordHash = await bcrypt.hash(password, salt)
+    console.log(passwordHash)
   } catch (err) {
     res.status(500).json(err)
   }
